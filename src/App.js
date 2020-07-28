c71b42c52ce85f91f338ea86b999d9179fc5329b
anviol <andrev.adm@gmail.com>
2020-07-27 21:52:33 -0300

    Inclusão do rodapé

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import dadosIniciais from './data/dados_iniciais.json'
 import Menu from './components/Menu'
 import BannerMain from './components/BannerMain'
 import Carousel from './components/Carousel'
-//import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -36,6 +36,8 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[4]}
       />
+
+      <Footer />
 
     </div>
   );
